59c5a843be5de05094b9ffb9e3ebb80b6e43871c
Bugama <bugama227@gmail.com>
2020-02-09 21:34:59 +0300

    Initial. SimpleCompare Done. UI NotDone

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,75 +30,55 @@
         {
             this.Orig_pb = new System.Windows.Forms.PictureBox();
             this.DownScaled_pb = new System.Windows.Forms.PictureBox();
-            this.Desaturated_pb = new System.Windows.Forms.PictureBox();
             this.ReducedColours_pb = new System.Windows.Forms.PictureBox();
-            this.final_pb = new System.Windows.Forms.PictureBox();
             this.Hash_label = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.Orig_pb)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.DownScaled_pb)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Desaturated_pb)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ReducedColours_pb)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.final_pb)).BeginInit();
             this.SuspendLayout();
             // 
             // Orig_pb
             // 
             this.Orig_pb.Location = new System.Drawing.Point(12, 28);
             this.Orig_pb.Name = "Orig_pb";
-            this.Orig_pb.Size = new System.Drawing.Size(382, 350);
+            this.Orig_pb.Size = new System.Drawing.Size(321, 499);
             this.Orig_pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Orig_pb.TabIndex = 0;
             this.Orig_pb.TabStop = false;
             // 
             // DownScaled_pb
             // 
-            this.DownScaled_pb.Location = new System.Drawing.Point(423, 28);
+            this.DownScaled_pb.Location = new System.Drawing.Point(339, 28);
             this.DownScaled_pb.Name = "DownScaled_pb";
-            this.DownScaled_pb.Size = new System.Drawing.Size(179, 162);
+            this.DownScaled_pb.Size = new System.Drawing.Size(251, 234);
             this.DownScaled_pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.DownScaled_pb.TabIndex = 1;
             this.DownScaled_pb.TabStop = false;
             // 
-            // Desaturated_pb
-            // 
-            this.Desaturated_pb.Location = new System.Drawing.Point(423, 216);
-            this.Desaturated_pb.Name = "Desaturated_pb";
-            this.Desaturated_pb.Size = new System.Drawing.Size(179, 162);
-            this.Desaturated_pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.Desaturated_pb.TabIndex = 2;
-            this.Desaturated_pb.TabStop = false;
-            // 
             // ReducedColours_pb
             // 
-            this.ReducedColours_pb.Location = new System.Drawing.Point(608, 28);
+            this.ReducedColours_pb.Location = new System.Drawing.Point(339, 281);
             this.ReducedColours_pb.Name = "ReducedColours_pb";
-            this.ReducedColours_pb.Size = new System.Drawing.Size(179, 162);
-            this.ReducedColours_pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.ReducedColours_pb.Size = new System.Drawing.Size(251, 246);
+            this.ReducedColours_pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.ReducedColours_pb.TabIndex = 3;
             this.ReducedColours_pb.TabStop = false;
-            // 
-            // final_pb
-            // 
-            this.final_pb.Location = new System.Drawing.Point(608, 216);
-            this.final_pb.Name = "final_pb";
-            this.final_pb.Size = new System.Drawing.Size(179, 162);
-            this.final_pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.final_pb.TabIndex = 4;
-            this.final_pb.TabStop = false;
             // 
             // Hash_label
             // 
             this.Hash_label.AutoSize = true;
-            this.Hash_label.Location = new System.Drawing.Point(605, 412);
+            this.Hash_label.Location = new System.Drawing.Point(497, 585);
             this.Hash_label.Name = "Hash_label";
             this.Hash_label.Size = new System.Drawing.Size(35, 13);
             this.Hash_label.TabIndex = 5;
@@ -116,34 +96,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(420, 12);
+            this.label2.Location = new System.Drawing.Point(339, 12);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(68, 13);
             this.label2.TabIndex = 7;
             this.label2.Text = "DownScaled";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(605, 12);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "ColourReduced";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(420, 200);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Desaturated";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(605, 200);
+            this.label5.Location = new System.Drawing.Point(339, 265);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 13);
             this.label5.TabIndex = 10;
@@ -152,7 +114,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(605, 393);
+            this.label6.Location = new System.Drawing.Point(607, 550);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(32, 13);
             this.label6.TabIndex = 11;
@@ -160,7 +122,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(111, 393);
+            this.button1.Location = new System.Drawing.Point(113, 550);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 12;
@@ -170,7 +132,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 393);
+            this.button2.Location = new System.Drawing.Point(14, 550);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 13;
@@ -178,43 +140,69 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // textBox1
             // 
-            this.button3.Location = new System.Drawing.Point(204, 393);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 14;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.textBox1.Location = new System.Drawing.Point(194, 550);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(645, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 585);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "label1";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(599, 28);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(239, 498);
+            this.listView1.TabIndex = 17;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "FirstImage";
+            this.columnHeader1.Width = 125;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "SecondImage";
+            this.columnHeader2.Width = 112;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(851, 607);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Hash_label);
-            this.Controls.Add(this.final_pb);
             this.Controls.Add(this.ReducedColours_pb);
-            this.Controls.Add(this.Desaturated_pb);
             this.Controls.Add(this.DownScaled_pb);
             this.Controls.Add(this.Orig_pb);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.Orig_pb)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DownScaled_pb)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Desaturated_pb)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ReducedColours_pb)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.final_pb)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -224,19 +212,19 @@
 
         private System.Windows.Forms.PictureBox Orig_pb;
         private System.Windows.Forms.PictureBox DownScaled_pb;
-        private System.Windows.Forms.PictureBox Desaturated_pb;
         private System.Windows.Forms.PictureBox ReducedColours_pb;
-        private System.Windows.Forms.PictureBox final_pb;
         private System.Windows.Forms.Label Hash_label;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -18,39 +19,159 @@ namespace ScaleTo16x16
         {
             InitializeComponent();
         }
-
+        public string[] Paths = { };
+        public Dictionary<string, string> Hashes = new Dictionary<string, string>();
+        public int DimensionScale = 32;
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
+            /*OpenFileDialog ofd = new OpenFileDialog();
 
-            if(ofd.ShowDialog() == DialogResult.OK)
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
+                List<string> Temp = Paths.ToList();
+                Temp.Add(ofd.FileName);
+                Paths = Temp.ToArray();
+                Hash_label.Text = Paths[i];
+                Hashes.Add(Paths[i], "");
                 Bitmap OrigImage = new Bitmap(ofd.FileName);
                 Orig_pb.Image = OrigImage;
                 Orig_pb.BackColor = Color.Black;
+            }*/
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                Hashes.Clear();
+                Paths = GetAllImagesPaths(fbd.SelectedPath);
             }
+        }
+
+        private string[] GetAllImagesPaths(string folderName)
+        {
+            var IEpaths = Directory.GetFiles(folderName, "*.*"). // search of typical image formats using lynq
+                Where(s => s.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                s.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+                s.EndsWith(".gif", StringComparison.OrdinalIgnoreCase) ||
+                s.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+                s.EndsWith(".tiff", StringComparison.OrdinalIgnoreCase) ||
+                s.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase));
+            string[] paths = IEpaths.ToArray();
+            return paths;
+        }
+
+        private void SetFingerPrintsIntoDictionary()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            string TempHash = "";
+            /*for (int i = 0; i < Paths.Length; i++)
+            {
+                TempHash = SetBlackAndWhite(Paths[i]);
+                Hashes.Add(Paths[i], TempHash);
+            }*/
+
+            Parallel.For(0, Paths.Length, i =>
+            {
+                TempHash = SetBlackAndWhite(Paths[i]);
+                Hashes.Add(Paths[i], TempHash);
+            });
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            Hash_label.Text = elapsedTime;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(Orig_pb.Image != null)
+            SetFingerPrintsIntoDictionary();
+            CompareFingerPrints();
+        }
+
+        private string SetBlackAndWhite(string path) 
+        {
+            Bitmap Temp = new Bitmap(path);
+            Bitmap BlackAndWhiteImage = new Bitmap(Temp, new Size(DimensionScale, DimensionScale));
+            Temp.Dispose();
+            Color pixel;
+            
+            string TempHash = "";
+            
+            for (int y = 0; y < BlackAndWhiteImage.Width; y++)
             {
-                Bitmap DownScaledImage = new Bitmap(Orig_pb.Image, new Size(16, 16));
-                DownScaled_pb.Image = DownScaledImage;
+                for(int x = 0; x < BlackAndWhiteImage.Height; x++)
+                {
+                    pixel = BlackAndWhiteImage.GetPixel(x, y);
+
+                    int a = pixel.A;
+
+                    int r = pixel.R;
+                    int g = pixel.G;
+                    int b = pixel.B;
+
+                    int avg = (r + g + b) / 3;
+                    if (avg > 170)
+                    {
+                        TempHash += "1";
+                    }
+                    else
+                    {
+                        TempHash += "0";
+                    }
+                }
+            }
+            
+            return TempHash;
+        }
+
+        private void CompareFingerPrints()
+        {
+            /*int i = 0, j = 1;
+            while(i < Hashes.Count - 1)
+            {
+                while(j < Hashes.Count)
+                {
+                    j++
+                }
+                i++;
+            }*/
+            int J = 1;
+            for(int i = 0; i < Hashes.Count - 1; i++)
+            {
+                for(int j = J; j < Hashes.Count; j++)
+                {
+                    bool isSimilar = AnotherCompareFunc(Hashes.ElementAt(i).Value, Hashes.ElementAt(j).Value);
+                    
+                    if(isSimilar == true)
+                    {
+                        string[] TemplviItem = { Path.GetFileName(Hashes.ElementAt(i).Key), Path.GetFileName(Hashes.ElementAt(j).Key) };
+                        ListViewItem lvi = new ListViewItem(TemplviItem);
+                        listView1.Items.Add(lvi);
+                    }
+                }
+                J++;
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private bool AnotherCompareFunc(string firstHash, string secondHash)
         {
-            if(DownScaled_pb.Image != null)
+            int DiffCount = 0;
+            for (int i = 0; i < DimensionScale * DimensionScale; i++)
             {
-                SaveFileDialog sfd = new SaveFileDialog();
-                if(sfd.ShowDialog() == DialogResult.OK)
+                if(DiffCount > 50)
                 {
-                    Bitmap saveds = new Bitmap(DownScaled_pb.Image);
-                    saveds.Save(sfd.FileName, ImageFormat.Jpeg);
+                    return false;
+                }
+                if(firstHash[i] != secondHash[i])
+                {
+                    DiffCount++;
                 }
             }
+            return true;
         }
     }
 }
